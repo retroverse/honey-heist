1926effaa5419a94b4ee682d579bf0344653cb11
retroverse <ebreakey@gmail.com>
2021-01-11 17:45:52 +1100

    Tidy CharacterBoxes and seperate traits

--- src/CharacterBoxes.js
+++ src/CharacterBoxes.js
@@ -1,61 +1,9 @@
 import React, { useState } from 'react'
 
-
+import {species, roles, skills, descriptors} from './traits'
 import { useQueryParam } from './hooks/queryParam'
 import './style/CharacterBoxes.css'
 
-const descriptors = [
-  'rookie',
-  'washed-up',
-  'retired',
-  'unhinged',
-  'slick',
-  'incompetent',
-  'psychic',
-  'arrogant',
-  'overzealous',
-  'clumsy'
-]
-
-const species = [
-  'grizzly bear',
-  'polar bear',
-  'panda',
-  'black bear',
-  'sun bear',
-  'honey badger',
-  'sloth bear',
-  'spectacled bear',
-  'brown bear',
-  'beaver'
-]
-
-const roles = [
-  'muscle',
-  'brains',
-  'driver',
-  'hacker',
-  'thief',
-  'face',
-  'gymnast',
-  'alcoholic',
-  'capitalist',
-  'daredevil',
-  'engineer'
-]
-
-const skills = {
-  'grizzly bear': 'Terrify',
-  'polar bear': 'Swim',
-  'panda': 'Charm',
-  'black bear': 'Climb',
-  'sun bear': 'Sense Honey',
-  'honey badger': 'Carnage',
-  'sloth bear': 'Stealth',
-  'spectacled bear': 'Enhanced Sight',
-  'brown bear': 'Fish',
-  'beaver': 'Gnaw'
-}
 
 const isVowel = str =>
     str == 'a' ||
@@ -72,13 +20,36 @@ const CharacterBoxes = ({}) => {
   const [speciesValue, setSpeciesValue] = useQueryParam('s', species[0])
   const [roleValue, setRoleValue] = useQueryParam('r', roles[0])
 
+  const nameInput = <input
+    type='text'
+    value={nameValue}
+    onChange={handleValueChange(setNameValue)}
+    placeholder='name'
+    className='character-box-field'/>
+
+  const descriptorInput = <FieldSelect
+    value={descriptorValue}
+    onChange={handleValueChange(setDescriptorValue)}
+    options={descriptors}/>
+
+  const speciesInput = <FieldSelect
+    value={speciesValue}
+    onChange={handleValueChange(setSpeciesValue)}
+    options={species}/>
+
+  const roleInput = <FieldSelect
+    value={roleValue}
+    onChange={handleValueChange(setRoleValue)}
+    options={roles}/>
+
   return <>
     <div className='character-box bear'>
       Hi!
-      I am <input type='text' value={nameValue} onChange={handleValueChange(setNameValue)} placeholder='name' className='character-box-field'/>. <br />
-      I am a{isVowel(descriptorValue[0]) && 'n'} <FieldSelect value={descriptorValue} onChange={handleValueChange(setDescriptorValue)} options={descriptors}/><span className='gap'/>
-      <FieldSelect value={speciesValue} onChange={handleValueChange(setSpeciesValue)} options={species}/>, <br />
-      the <FieldSelect value={roleValue} onChange={handleValueChange(setRoleValue)} options={roles}/> of the operation.
+      I am {nameInput}.<br />
+      I am a{isVowel(descriptorValue[0]) && 'n'} {descriptorInput}
+        <span className='gap'/>
+        {speciesInput}, <br />
+      the {roleInput} of the operation.
     </div>
     <div className='character-box skill-box bear'>
       <p>As a <span className='species-label'>{speciesValue}</span> you can use</p>
---
+++ src/traits.js
@@ -0,0 +1,53 @@
+export const descriptors = [
+  'rookie',
+  'washed-up',
+  'retired',
+  'unhinged',
+  'slick',
+  'incompetent',
+  'psychic',
+  'arrogant',
+  'overzealous',
+  'clumsy'
+]
+
+export const species = [
+  'grizzly bear',
+  'polar bear',
+  'panda',
+  'black bear',
+  'sun bear',
+  'honey badger',
+  'sloth bear',
+  'spectacled bear',
+  'brown bear',
+  'beaver'
+]
+
+export const roles = [
+  'muscle',
+  'brains',
+  'driver',
+  'hacker',
+  'thief',
+  'face',
+  'gymnast',
+  'alcoholic',
+  'capitalist',
+  'daredevil',
+  'engineer'
+]
+
+export const skills = {
+  'grizzly bear': 'Terrify',
+  'polar bear': 'Swim',
+  'panda': 'Charm',
+  'black bear': 'Climb',
+  'sun bear': 'Sense Honey',
+  'honey badger': 'Carnage',
+  'sloth bear': 'Stealth',
+  'spectacled bear': 'Enhanced Sight',
+  'brown bear': 'Fish',
+  'beaver': 'Gnaw'
+}
+
